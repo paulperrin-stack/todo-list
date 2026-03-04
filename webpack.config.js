dea19a1bba5464a2e962dab02e5e474dfedac218
Paul <247144100+paulperrin-stack@users.noreply.github.com>
2026-03-04 14:51:55 +0700

    build: enable CSS handling in webpack with style-loader and css-loader

--- webpack.config.js
+++ webpack.config.js
@@ -6,13 +6,13 @@ module.exports = {
     output: {
         filename: 'main.js', // Bundled output file
         path: path.resolve(__dirname, 'dist'), // Output directory
+        clean: true, // good addition: cleans dist folder before each build
     },
-    mode: 'development', // Change to 'production' later for minification
-    /*
+    mode: 'development',
     module: {
         rules: [
             {
-                test: /\.js$/, // For JS files
+                test: /\.js$/,
                 exclude: /node_modules/,
                 use: {
                     loader: 'babel-loader',
@@ -21,25 +21,30 @@ module.exports = {
                     },
                 },
             },
+            {
+                test: /\.css$/i,
+                use: [
+                    'style-loader', // Injects CSS into the DOM (<style> tag)
+                    'css-loader'    // Interprets @import/url() and resolves paths
+                ],
+            },
         ],
-    }, */
+    },
+
     devServer: {
         static: {
-            directory: path.join(__dirname, 'dist'), // Serve from dist
-            },
-            compress: true,
-            port: 9000, // Or any port
-            client: {
-                logging: 'warn',
-                overlay: true,
-            }
-            hot: true, // Hot module replacement
-            open: true,
+            directory: path.join(__dirname, 'dist'),
+        },
+        compress: true,
+        port: 9000,
+        open: true,     // auto-opens browser
+        hot: true,      // uncommented - enables hot module replacement
     },
+
     plugins: [
         new HtmlWebpackPlugin({
-            template: 'index.html',
+            template: './src/index.html',
             filename: 'index.html',
         })
     ]
-};
+}
